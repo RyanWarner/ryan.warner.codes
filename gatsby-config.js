26ad89a7ba5b3e90b716f612374a061881cb9465
Ryan Warner <warner.ryan@gmail.com>
2019-12-29 15:35:58 -0500

    Testing with gatsby-plugin-amplitude

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,15 @@
 const path = require('path')
 
+const paths = require('./src/config/paths')
+require('dotenv').config({
+  path: `${paths.dotenv}.${process.env.DEPLOY_ENV || 'staging'}`
+})
+
+const amplitudeApiKey = process.env.AMPLITUDE_API_KEY
+console.log('process.env.DEPLOY_ENV', process.env.DEPLOY_ENV)
+console.log('amplitudeApiKey', amplitudeApiKey)
+console.log('paths.dotenv', paths.dotenv)
+
 module.exports = {
   siteMetadata: {
     title: 'Ryan Warner',
@@ -44,7 +54,28 @@ module.exports = {
         display: 'minimal-ui',
         icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       }
-    }
+    }, {
+      resolve: `gatsby-plugin-amplitude-analytics`,
+      options: {
+        // Specify the API key for your Amplitude Project (required)
+        apiKey: '4c24c0a86063468a5c71c19abfca0c39',
+        // Puts tracking script in the head instead of the body (optional)
+        head: false,
+        // Prevents loading Amplitude and logging events if visitors have "Do Not Track" enabled (optional)
+        // respectDNT: true,
+        // Override the default event types (optional)
+        eventTypes: {
+          outboundLinkClick: 'outbound.link.click',
+          pageView: 'page.view',
+        },
+        // Amplitude JS SDK configuration options (optional)
+        amplitudeConfig: {
+          saveEvents: true,
+          includeUtm: true,
+          includeReferrer: true
+        }
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
--- src/config/paths.js
+++ src/config/paths.js
@@ -5,7 +5,7 @@ const appDirectory = fs.realpathSync(process.cwd())
 const resolveApp = (relativePath) => path.resolve(appDirectory, relativePath)
 
 const paths = {
-  dotenv: resolveApp('config/.env'),
+  dotenv: resolveApp('src/config/.env'),
   src: resolveApp('src'),
   nodeModules: resolveApp('node_modules')
 }
