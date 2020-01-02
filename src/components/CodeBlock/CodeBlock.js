8dc8c8d6f30127da4c3243fb63ffd04acbfacb61
Ryan Warner <warner.ryan@gmail.com>
2020-01-02 11:18:24 -0500

    Fix padding issue inside code blocks

--- src/components/CodeBlock/CodeBlock.js
+++ src/components/CodeBlock/CodeBlock.js
@@ -2,24 +2,28 @@ import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
 import theme from 'prism-react-renderer/themes/oceanicNext'
 
+import * as S from './styles'
+
 export default ({ children, className }) => {
   const language = className ? className.replace(/language-/, '') : 'js'
 
   return (
     <Highlight {...defaultProps} code={children} language={language} theme={theme}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px' }}>
-          {tokens.map((line, i) => {
-            if (i === tokens.length - 1 && line[0].empty) return null
-            return (
-              <div key={i} {...getLineProps({ line, key: i })}>
-                {line.map((token, key) => (
-                  <span key={key} {...getTokenProps({ token, key })} />
-                ))}
-              </div>
-            )
-          })}
-        </pre>
+        <S.Pre className={className} style={{ ...style }}>
+          <S.Code>
+            {tokens.map((line, i) => {
+              if (i === tokens.length - 1 && line[0].empty) return null
+              return (
+                <div key={i} {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              )
+            })}
+          </S.Code>
+        </S.Pre>
       )}
     </Highlight>
   )
---
+++ src/components/CodeBlock/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const Pre = styled.pre`
+  overflow-x: scroll;
+  border-radius: 4px;
+`
+
+export const Code = styled.code`
+  margin: 20px;
+  display: inline-block;
+  min-width: 100%;
+`
