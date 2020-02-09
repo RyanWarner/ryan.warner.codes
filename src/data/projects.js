7013e4ca1585d79ecf4485a129e994c393383512
Ryan Warner <warner.ryan@gmail.com>
2020-02-09 10:51:59 -0500

    Add prettierrc and reorder projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,23 +1,26 @@
 import { Colors } from 'styles'
 
 export default {
+  at: {
+    title: 'All Turtles',
+    description:
+      'I lead web development full time at All Turtles, an AI startup studio. I work across many projects, building React applications from scratch on top of varying types of servers and databases.',
+    url: 'https://all-turtles.com',
+    color: Colors.allTurtles,
+    span: 2
+  },
   yawnch: {
     title: 'Yawnch',
-    description: 'An online coworking community for indie hackers, entreprenuers, and founders. Chat live with app developers, designers, and mentors. Access video courses and tutorials.',
+    description:
+      'An online coworking community for indie hackers, entreprenuers, and founders.',
     url: 'https://yawnch.com',
-    color: Colors.yawnch,
-    span: 2
+    color: Colors.yawnch
   },
   sstv: {
     title: 'Stockstream.tv',
-    description: 'On a mission to democratize investing knowledge. My partner and I bootstrapped this company with zero funding.',
+    description:
+      'On a mission to democratize investing knowledge. My partner and I bootstrapped this company with zero funding.',
     url: 'https://stockstream.tv',
     color: Colors.stockstreamtv
-  },
-  at: {
-    title: 'All Turtles',
-    description: 'I lead web development full time at All Turtles, an AI startup studio.',
-    url: 'https://all-turtles.com',
-    color: Colors.allTurtles
   }
 }
