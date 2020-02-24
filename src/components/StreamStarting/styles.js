f750940389ac0a38d08414e909a3f88b07c39806
Ryan Warner <warner.ryan@gmail.com>
2020-02-24 12:23:14 -0500

    Start on stream scenes

---
+++ src/components/StreamStarting/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const StreamStarting = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(162.07deg, #BAFFC5 5.07%, #74AED6 80.61%);
+`
