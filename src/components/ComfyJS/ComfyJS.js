bf2dc51d3b8e85e3a7a07197a2737207f77edf40
Ryan Warner <warner.ryan@gmail.com>
2020-03-08 19:21:51 -0400

    Move game start log into func

--- src/components/ComfyJS/ComfyJS.js
+++ src/components/ComfyJS/ComfyJS.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import ComfyJS from 'comfy.js'
 
 import * as S from './styles'
@@ -7,7 +7,9 @@ import Fireball from './Fireball'
 const commands = {
   test: 'test',
   heart: 'heart',
-  fireball: 'fireball'
+  heal: 'heal',
+  fireball: 'fireball',
+  hadouken: 'hadouken'
 }
 
 ComfyJS.Init('RyanWarnerCodes')
@@ -18,21 +20,7 @@ function getRandomInt(min, max) {
 
 let lasers = []
 
-function gameLoop() {
-  for (var i = 0; i < lasers.length; i++) {
-    var laser = lasers[i]
-    const doMovement = laser.doMovement()
 
-    switch (doMovement) {
-      case 'delete':
-        lasers.splice(i, 1)
-        break
-      case 'hit':
-        lasers.splice(i, 1)
-        break
-    }
-  }
-}
 
 const addNew = () => {
   const laser = new Fireball({
@@ -46,42 +34,47 @@ const addNew = () => {
   lasers.push(laser)
 }
 
-window.setInterval(gameLoop, 33)
+
+
+const startGame = () => {
+  function gameLoop() {
+    for (var i = 0; i < lasers.length; i++) {
+      var laser = lasers[i]
+      const doMovement = laser.doMovement()
+
+      switch (doMovement) {
+        case 'delete':
+          lasers.splice(i, 1)
+          break
+        case 'hit':
+          lasers.splice(i, 1)
+          break
+      }
+    }
+  }
+
+  window.setInterval(gameLoop, 33)
+}
+
+startGame()
 
 export default props => {
   const [hearts, setHearts] = useState(1)
-
-  // useEffect(() => {
-  //   function gameLoop() {
-  //     for (var i = 0; i < lasers.length; i++) {
-  //       var laser = lasers[i]
-  //       if (laser.doMovement() === "delete") {
-  //         lasers.splice(i, 1)
-  //       }
-
-  //       if (laser.doMovement() === "hit") {
-  //         lasers.splice(i, 1)
-  //       }
-  //     }
-  //   }
-
-  //   window.setInterval(gameLoop, 1)
-  // }, [])
 
   ComfyJS.onCommand = (user, command, message, flags, extra) => {
     if (flags.broadcaster && command === commands.test) {
       console.log('!test was typed in chat')
     }
 
-    if (command === commands.heart) {
+    if (command === commands.heal) {
       setHearts(hearts + 1)
     }
 
     if (command === commands.fireball) {
-      if (hearts > 1) setHearts(hearts - 1)
       addNew()
     }
   }
+
   const newArray = Array(hearts).fill().map((x, i) => i)
   return (
     <S.ComfyJSComponent>
