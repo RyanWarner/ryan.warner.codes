279d4a4b5d27134fa8377db0a9e1845cb8c4dbc7
Ryan Warner <warner.ryan@gmail.com>
2020-04-11 10:28:40 -0400

    Update nav items

--- src/data/navItems.js
+++ src/data/navItems.js
@@ -1,18 +1,47 @@
 export default {
-  articles: {
-    title: 'Articles',
-    url: '/articles'
+  tutorials: {
+    title: 'Tutorials',
+    url: '/tutorials'
   },
-  notes: {
-    title: 'Notes',
-    url: '/notes'
+  resources: {
+    title: 'Resources',
+    url: '/resources'
   },
-  about: {
-    title: 'About',
-    url: '/about'
+  courses: {
+    title: 'Courses',
+    url: '/courses'
+  },
+  workshops: {
+    title: 'Workshops',
+    url: '/workshops'
+  },
+  feedback: {
+    title: 'Feedback',
+    url: '/feedback'
   },
   mentorship: {
     title: 'Mentorship',
     url: '/mentorship'
+  },
+  live: {
+    title: 'Live',
+    url: '/live'
   }
 }
+
+const sideNavItems = {
+  notes: {
+    title: 'Notes',
+    url: '/notes'
+  },
+  uses: {
+    title: 'Uses',
+    url: '/uses'
+  },
+  about: {
+    title: 'About',
+    url: '/about'
+  }
+}
+
+export { sideNavItems }
