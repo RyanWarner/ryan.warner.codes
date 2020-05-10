9e3ceafd93291a3bdfc0186d647ed5572475174e
Ryan Warner <warner.ryan@gmail.com>
2020-05-10 13:43:59 -0400

    Update slack webhook url

--- functions/stripe/index.js
+++ functions/stripe/index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const { IncomingWebhook } = require('@slack/webhook')
-const slackWebhookUrl = 'https://hooks.slack.com/services/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'
+const slackWebhookUrl = 'https://hooks.slack.com/services/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'
 
 const webhook = new IncomingWebhook(slackWebhookUrl, {
   icon_emoji: ':moneybag:',
@@ -16,8 +16,9 @@ app.use(bodyParser.json())
 
 app.post('/', async (req, res) => {
   const { body } = req
-  console.info('req.stripeEventType', body.type)
   const email = body.data.object.billing_details.email
+  console.info('req.stripeEventType', body.type)
+  console.info('email', email)
 
   await webhook.send({
     blocks: [
