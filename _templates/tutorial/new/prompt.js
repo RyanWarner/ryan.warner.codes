659726570617bcffd42721e7cd00ab41a9d0c9e7
Ryan Warner <warner.ryan@gmail.com>
2020-05-22 12:08:56 -0400

    Create a hygen generator for tutorial.mdx files

---
+++ _templates/tutorial/new/prompt.js
@@ -0,0 +1,23 @@
+module.exports = [
+  {
+    type: 'input',
+    name: 'url',
+    message: 'URL'
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Title'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Description'
+  },
+  {
+    type: 'select',
+    name: 'icon',
+    message: 'Icon',
+    choices: ['react', 'figma', 'vscode', 'gatsby']
+  }
+]
---
+++ _templates/tutorial/new/tutorial.ejs.t
@@ -0,0 +1,13 @@
+---
+to: src/content/articles/<%= url %>/index.mdx
+---
+---
+title: <%= title %>
+description: <%= description %>
+date: 1567987200
+icon: <%= icon %>
+type: article
+ogImage: ''
+tags:
+  - react
+---
