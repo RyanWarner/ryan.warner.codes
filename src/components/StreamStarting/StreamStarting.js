aad1f7d3d2f6ce20451b09c23444455d2c2f3ac4
Ryan Warner <warner.ryan@gmail.com>
2020-05-28 10:31:41 -0400

    Update stream title

--- src/components/StreamStarting/StreamStarting.js
+++ src/components/StreamStarting/StreamStarting.js
@@ -6,7 +6,7 @@ import { Timer } from '../'
 import { Time } from '../Icons'
 import hearts from './hearts'
 
-const title = 'Continuing work on the All Turtles Gatsby site (afternoon stream)'
+const title = 'Cranking on the All Turtles Gatsby site'
 const date = dayjs().format('dddd, MMM DD YYYY')
 
 export default () => {
