bf7f1c1137cbe93958cb2920c4be6595c2a6e246
Ryan Warner <warner.ryan@gmail.com>
2020-06-09 09:51:18 -0400

    Update deps

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,8 +106,5 @@ module.exports = {
         }
       }
     }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
   ]
 }
