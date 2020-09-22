8092fb33af9f274c7a732a4c4c935b1eb79b630e
Ryan Warner <warner.ryan@gmail.com>
2020-09-22 19:16:40 -0400

    Remove boilerplate content

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,22 +1,22 @@
 const typeTokens = {
-  fontFamily: 'Lato, sans-serif'
+  fontFamily: 'Clear Sans, sans-serif'
 }
 
 export default {
   light: {
     ...typeTokens,
-    background: '#FBFBF9',
-    text10: '#1E1E1B',
-    text20: '#696966',
+    background: '#FCFCF9',
+    text10: '#313F47',
+    text20: '#485D69',
     rule: 'rgba(0, 0, 0, 0.1)',
     green10: '#79BB40',
     highlightBackground: '#ABEF70'
   },
   dark: {
     ...typeTokens,
-    background: '#171716',
-    text10: '#FBFBF9',
-    text20: '#9C9C96',
+    background: '#161E25',
+    text10: '#E9F7FF',
+    text20: '#C9E2F0',
     rule: 'rgba(255, 255, 255, 0.1)',
     green10: '#79BB40',
     highlightBackground: '#ABEF70'
